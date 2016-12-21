d3414833b333dd8b2c5da164586ff412eaadcc59
Max Wofford <max@maxwofford.com>
2016-12-20 23:46:03 -0800

    Add logic inside loop

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,5 @@
+const TURN_NUMBER = 5;
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete" && document.getElementById("game-page") !== null) {
@@ -9,25 +11,49 @@ chrome.extension.sendMessage({}, function(response) {
 });
 
 
-var loopInterval;
+var loopInterval, turn;
+var prev = {};
+
 function init(){
   // ----------------------------------------------------------
 	// This part of the script triggers when page is done loading and the game starts
 	console.log("Hello. This message was sent from scripts/inject.js");
 	// ----------------------------------------------------------
 
-  
+
 }
 
 function loop(){
-  
-  console.log("test");
+  var rows = document.getElementById("game-leaderboard").children[0].children;
+  if (turn === document.querySelector("#turn-counter").innerText) {
+    return;
+  }
+  turn = document.querySelector("#turn-counter").innerText;
+  for(let row of rows){
+    let name = row.children[1].textContent;
+    if(name === "Player"){
+      continue;
+    }
+    let army = +row.children[2].textContent;
+    let color = row.children[1].className.split(' ')[1];
+    let prevArmy = prev[color] || 0;
+
+    prev[color] = army;
+
+    td = document.createElement('td');
+    msg = document.createTextNode(army - prevArmy);
+    row.appendChild(td);
+    td.appendChild(msg);
+    if (row.children.length > 4 + TURN_NUMBER) {
+      row.removeChild(row.children[4]);
+    }
+  }
 }
 
 
 
 function end(){
-  
-  
+
+
   clearInterval(loopInterval);
 }
