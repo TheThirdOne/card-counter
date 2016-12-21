58f3965a9df312ce52c6c6362afe5ef1a80d6164
Benjamin Landers <benjaminrlanders@gmail.com>
2016-12-20 23:20:16 -0800

    break out init, loop and end methods

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,33 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
+	if (document.readyState === "complete" && document.getElementById("game-page") !== null) {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+		init();
+		loopInterval = setInterval(loop, 100);
 	}
 	}, 10);
 });
+
+
+var loopInterval;
+function init(){
+  // ----------------------------------------------------------
+	// This part of the script triggers when page is done loading and the game starts
+	console.log("Hello. This message was sent from scripts/inject.js");
+	// ----------------------------------------------------------
+
+  
+}
+
+function loop(){
+  
+  console.log("test");
+}
+
+
+
+function end(){
+  
+  
+  clearInterval(loopInterval);
+}
